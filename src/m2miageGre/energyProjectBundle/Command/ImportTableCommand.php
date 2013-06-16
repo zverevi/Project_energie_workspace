e639cad1cd25c1785b1930e5d9939c716dc216a0
Paul Valla <paul.valla@gmail.com>
2013-06-16 16:18:44 +0200

    create irise file and php model

--- src/m2miageGre/energyProjectBundle/Command/ImportTableCommand.php
+++ src/m2miageGre/energyProjectBundle/Command/ImportTableCommand.php
@@ -1,15 +1,30 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: paul
- * Date: 16/06/13
- * Time: 15:51
- * To change this template use File | Settings | File Templates.
- */
 
 namespace m2miageGre\energyProjectBundle\Command;
 
 
-class ImportTableCommand {
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+class ImportTableCommand extends ContainerAwareCommand {
+
+    protected function configure()
+    {
+        $this
+            ->setName("irise:createTable")
+            ->setDescription("Extract Data from Irise file and populate DataBase")
+            ->addArgument(
+                "f",
+                InputArgument::REQUIRED,
+                "File path of Irise datafile"
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filePath = $input->getArgument('f');
+
+    }
 }
