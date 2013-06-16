ea63960ca9b73b6186ec1a4062f4dc94eef46c15
Paul Valla <paul.valla@gmail.com>
2013-06-16 11:54:52 +0200

    switch to php

--- DBEnergyManager/BD/net.sql
+++
@@ -1,7 +0,0 @@
-delete from suivi_releve;
-delete from maison;
-delete from equipement;
-delete from projet;
-commit;
-
-@seqIncAll.sql
--- DBEnergyManager/src/modele/inputmanagment/DataLoaderInterface.java
+++
@@ -1,6 +0,0 @@
-package modele.inputmanagment;
-
-public interface DataLoaderInterface {
-	public int afficherTousFichiersDossier(Dossier D);
-	public void lireFichier(int numFichier, Dossier D);
-}
